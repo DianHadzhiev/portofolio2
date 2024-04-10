93ed82deb4b5e6b36d8369216893a555a370d40e
DianHadzhiev <dido.hadzhiev17@gmail.com>
2024-04-10 14:31:19 +0200

    yes

--- portofolio2/app/src/main/java/com/portofolio2/Aandeel.java
+++ portofolio2/app/src/main/java/com/portofolio2/Aandeel.java
@@ -1,25 +1,5 @@
 package com.portofolio2;
 
 public class Aandeel {
-    private String naam;
-    private double prijs;
-    
-   
-    public String getNaam() {
-        return naam;
-    }
-    public void setNaam(String naam) {
-        this.naam = naam;
-    }
-
-    public double getPrijs() {
-        return prijs;
-    }
-    public void setPrijs(double prijs) {
-        this.prijs = prijs;
-    }
-
     
 }
-
-
